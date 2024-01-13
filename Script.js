6fbded27a242f123ec28cb6175d0e6bfb407e8e7
Hammadullah Khan <hammadullahk.bscssef20@iba-suk.edu.pk>
2024-01-13 23:30:26 +0500

    adjust indentation

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 console.log("movin to the new customer");
-fetch('https://randomuser.me/api/?result=1')
-.then(response => response.json())
-.then(data =>console.log(data));
-console.log(`our valued customer, please give me some time while i get some information back from the Records dept.z`)
+fetch("https://randomuser.me/api/?result=1")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+console.log(
+  `our valued customer, please give me some time while i get some information back from the Records dept.z`
+);
