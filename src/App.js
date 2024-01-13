4d81abc1b3c555a2804c9f6a4c0500826c5bdf45
Hammadullah Khan <hammadullahk.bscssef20@iba-suk.edu.pk>
2024-01-14 00:23:42 +0500

    implement Fetch API in React app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [btcData, setBtcData] = useState({});
+  useEffect(() => {
+    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`) 
+    .then((response) => response.json())
+    .then((jsonData) => setBtcData(jsonData.bpi.USD))
+    
+
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current BTC DATA</h1>
+      <p>Code: {btcData.code}</p>
+      <p>Symbol: {btcData.symbol}</p>
+      <p>Rate: {btcData.rate}</p>
+      <p>Description: {btcData.discription}</p>
+      <p>Rate Float: {btcData.rate_float}</p> 
+
     </div>
   );
 }
