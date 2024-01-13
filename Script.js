b122baf776962d82010fa647f4514ae6a3c2e04c
Hammadullah Khan <hammadullahk.bscssef20@iba-suk.edu.pk>
2024-01-13 23:11:32 +0500

    add fetch API

---
+++ Script.js
@@ -0,0 +1,5 @@
+console.log("movin to the new customer");
+fetch('https://randomuser.me/api/?result=1')
+.then(response => response.json())
+.then(data =>console.log(data));
+console.log(`our valued customer, please give me some time while i get some information back from the Records dept.z`)
